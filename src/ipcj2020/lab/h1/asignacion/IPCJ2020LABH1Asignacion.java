0a61fc40175abc5d20aeb8aafec7a1a6a93c575b
Alejandro <nandrez2298@gmail.com>
2020-06-03 12:07:46 -0600

    Mi primer commodr your changes. Lines starting

---
+++ src/ipcj2020/lab/h1/asignacion/IPCJ2020LABH1Asignacion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipcj2020.lab.h1.asignacion;
+
+/**
+ *
+ * @author Samu
+ */
+public class IPCJ2020LABH1Asignacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
